cf15af57b80c0d3063c94b7e10705d2260bf710a
Tom Hughes <tomhughes1987@gmail.com>
2022-05-16 15:23:28 +0100

    fix the first few test of the title in the index.html

--- index.test.js
+++ index.test.js
@@ -1,11 +1,6 @@
-
-/**
-* @jest-environment jsdom
-*/
-
 const fs = require("fs");
 const path = require("path");
-const html = fs.readFileSync(path.resolve(__dirname, "../index.html"), "utf8");
+const html = fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf8");
 
 describe("Testing Client-Side index.html...", () => {
   beforeAll(() => {
@@ -13,9 +8,22 @@ describe("Testing Client-Side index.html...", () => {
     console.log("Testing server is operational");
   });
 
+
+//My tests of the title
+// change the title of the html file and run npm test to see if the tests responde correctly
   it("check title has value", () => {
     let docTitle = document.title;
     expect(docTitle).not.toBe("");
   });
+
+  it("check title ", () => {
+    let docTitle = document.title;
+    expect(docTitle).not.toBe("Cars");
+  });
+  it("check title has value", () => {
+    let docTitle = document.title;
+    expect(docTitle).toBe("Document");
+  });
+////////////////////////////////////////////////////////////////////////////////
   
 }); // END OF DESCRIBE
