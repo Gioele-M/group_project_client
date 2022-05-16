5409bc68ee0d511ae010170e6b0648088c97c91c
Tom Hughes <tomhughes1987@gmail.com>
2022-05-16 15:15:53 +0100

    install jest enviroment jdom to try fix html jest tests

--- index.test.js
+++ index.test.js
@@ -1,11 +1,13 @@
+
+/**
+* @jest-environment jsdom
+*/
+
 const fs = require("fs");
 const path = require("path");
-const html = fs.readFileSync("/index.html", "utf8");
-// const html = fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf8");
+const html = fs.readFileSync(path.resolve(__dirname, "../index.html"), "utf8");
 
-// const card = document.getElementById("mainComment")
-
-describe("Testing index.html...", () => {
+describe("Testing Client-Side index.html...", () => {
   beforeAll(() => {
     document.documentElement.innerHTML = html.toString();
     console.log("Testing server is operational");
@@ -17,26 +19,3 @@ describe("Testing index.html...", () => {
   });
   
 }); // END OF DESCRIBE
-
-
-
-// console.log (document.documentElement.innerHTML = html.toString())
-
-
-
-
-
-
-// const fs = require('fs');
-// const path = require('path');
-// const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
-// jest.dontMock('fs');
-// describe('$', function () {
-//     beforeEach(() => {
-//         document.documentElement.innerHTML = html.toString();
-//     });
-//     afterEach(jest.resetModules);
-//     it('finds a node', function () {
-//         expect($('el')).toBeInTheDocument()
-//     });
-// });
