cc497a1aa6b48d2dbb2048d40fb8406f07bc568a
n451m <nasim.a.bilal@gmail.com>
2022-05-17 14:31:15 +0100

    created close function on the modal

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 let modalBtn = document.querySelector(".modal-button")
 let modalBg = document.querySelector(".modal-bg")
+let modalClose = document.querySelector(".modal-close")
 
 modalBtn.addEventListener('click',function(){
     console.log('has been clicked') 
     modalBg.classList.add('bg-active');
+});
+
+modalClose.addEventListener('click',function(){
+    console.log('has been clicked') 
+    modalBg.classList.remove('bg-active');
 });
