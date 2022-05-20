b7711e9d842e4f7065994e7985d14db9c69ebdff
Gioele-M <gioelemontis97@gmail.com>
2022-05-20 09:15:42 +0100

    added tom's changes

--- reference.js
+++ reference.js
@@ -143,7 +143,39 @@ searchBtn.addEventListener("click", e => {
 
 
 
+//////////////////////////////////
+//add dark mode
+/////////////////////////////////
+const darkbtn = document.getElementById("darkmodebtn")
+let darkOnChecker = false
 
+darkbtn.addEventListener('click',function(){
+    if (darkOnChecker === false){
+    console.log('dark mode  clicked') 
+    document.body.style.backgroundColor="black"
+    document.body.style.color="white"
+    darkOnChecker = true
+}
+else {
+    console.log('dark mode  clicked') 
+    document.body.style.backgroundColor="white"
+    document.body.style.color="black"
+    darkOnChecker = false
+    }
+   
+
+})
+
+//////////////////////////////////
+//add about
+/////////////////////////////////
+const aboutbtn = document.getElementById("aboutbtn")
+
+aboutbtn.addEventListener('click',function(){
+    
+    console.log('about clicked') 
+    alert("THIS IS MADE BY NASIM, TOM, REBECCA, GIO");
+})
 
 
 
